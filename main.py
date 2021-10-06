57b2169e7d82c94854465f4480ff87086e20b907
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-10-07 01:22:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,37 +37,7 @@ async def start(bot, update):
     )
 
 
-@Bot.on_message(filters.private & filters.photo)
-async def glitch_art(bot, update):
-    download_path = PATH + "/" + str(update.from_user.id) + "/"
-    download_location = download_path + "photo.jpg"
-    message = await update.reply_text(
-        text="`Processing...`",
-        quote=True
-    )
-    try:
-        await update.download(
-            file_name=download_location
-        )
-    except Exception as error:
-        await message.edit_text(
-            text=f"**Error :** `{error}`\nContact @TheFayas."
-        )
-        return 
-    await message.edit_text(
-        text="`Converting to glitch...`"
-    )
-    try:
-        glitch_art = glitchart.jpeg(download_location)
-        await update.reply_photo(photo=glitch_art, quote=True)
-        os.remove(download_location)
-        os.remove(glitch_art)
-    except Exception as error:
-        await message.edit_text(
-            text=f"**Error :** `{error}`\nContact @TheFayas."
-        )
-        return
-    await message.delete()
+
 
 
 Bot.run()
