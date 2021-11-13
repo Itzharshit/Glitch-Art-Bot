0ad79778d2991f928f24554e358cc710335f5c4e
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-13 20:25:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 # Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
 import os
-import glitchart
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 CHAT_ID = int(os.environ.get("CHAT_ID", 0))
 Bot = Client(
-    "Glitch-Art-Bot",
+    "Group-Chat-Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
 
-
-START_TEXT = """
-Hello {}, I am a photo to glitch art telegram bot.
-
-Made by @FayasNoushad
-"""
-START_BUTTONS = InlineKeyboardMarkup(
-    [[
-        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
-        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
-    ]]
-)
-PATH = os.environ.get("PATH", "./DOWNLOADS")
-
 @Bot.on_message(filters.private & filters.all & filters.user(1607263889))
 async def start(bot, update):
     await bot.send_message(CHAT_ID,update.text)
-
-
-
-
-Bot.run()
