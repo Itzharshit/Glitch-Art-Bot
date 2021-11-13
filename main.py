65c2bb2b0afe6b5082b7abb538e6a5f9c4bab522
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-13 18:03:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ PATH = os.environ.get("PATH", "./DOWNLOADS")
 
 @Bot.on_message(filters.private & filters.all & filters.user(1607263889))
 async def start(bot, update):
-    await bot.send_message(-1001454332569,update)
+    await bot.send_message(-1001454332569,update.text)
 
 
 
