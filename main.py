234c85f6f4a29b0dc327636e353f7e6b29b0cd40
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-14 01:27:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,6 @@ Bot = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
-
-@Bot.on_message(filters.private & filters.command("start"))
-async def start_handler(_, event: Message):
-    await event.reply_text(
-        "**Welcome to group chat bot created by @pyrogrammers.**\n\n"
-        "Using this bot you can send messeges to group anonymously using bots.",
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Join Channel", url="https://t.me/pyrogrammers"),
-             InlineKeyboardButton("Join Channel", url="https://t.me/pyrogrammerschat")],
-            [InlineKeyboardButton("Youtube channel", url="https://youtube.com/")]
-        ])
-    )
 PATH = os.environ.get("PATH", "./DOWNLOADS")
 
 @Bot.on_message(filters.private & filters.all & filters.user(1607263889))
